5e144fb85c111234daec26f7dd3a6f60697a51f2
devikabhatt1223 <devikabhatt1223@gmail.com>
2025-09-13 17:52:17 +0800

    test commit

---
+++ src/declarations.d.ts
@@ -0,0 +1,18 @@
+// src/declarations.d.ts
+
+declare module '*.svg' {
+  import * as React from 'react';
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
+
+declare module '*.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
+
+declare module '*.module.css' {
+  const classes: { [key: string]: string };
+  export default classes;
+}
